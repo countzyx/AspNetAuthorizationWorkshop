511680c67c137085c73db2602913f38819b1d181
Pedro R. Jiménez <pedro@thesafetytag.com>
2017-09-20 03:13:31 -0400

    Setup Authorization
    The app now returns are 401 when hitting the home page.

--- src/AuthorizationLab/Startup.cs
+++ src/AuthorizationLab/Startup.cs
@@ -17,6 +17,7 @@ namespace AuthorizationLab
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
+            services.AddAuthorization();
             services.AddMvc();
         }
 
